e266e93b1eea3d68831c793f9071195c7ebc8020
sac-nicte <karymnict@gmail.com>
2014-02-19 19:55:47 -0600

    Pk_2
    Pk_2

---
+++ Pk2_RSVPwapp/Pk2_RSVPwapp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pk2_RSVPwapp.Models;// se  importara el espacio de nombre 
+
+namespace Pk2_RSVPwapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ?
+                "Buenos Dias!!!" : "Buena Tarde!!!";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            return View("agradecimientoss", guestResponse);
+        }
+    }
+}
---
+++ holaController/holaController/Controllers/hiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace holaController.Controllers
+{
+    public class hiController : Controller
+    {
+        //
+        // GET: /hi/
+
+        public ActionResult Index()
+        {
+           hola mundo
+            return View();
+        }
+
+    }
+}
