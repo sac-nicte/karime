0eb77f2ed4bae04292786179a60288a3e5dc2d6f
sac-nicte <karymnict@gmail.com>
2014-02-25 21:09:04 -0600

    evaluacion_1
    evaluacion_1

---
+++ evaluacionU1/Pk2_RSVPwapp/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// usando p validar campos 
+using System.ComponentModel.DataAnnotations;
+
+namespace Pk2_RSVPwapp.Models
+{
+    public class GuestResponse
+    {
+        [Required (ErrorMessage="por favor ingrese su nombre.")]
+        public string Name { get; set;}
+
+        [Required(ErrorMessage = "por favor ingrese su correo electronico.")]
+        [RegularExpression (".+\\@.+\\..+",ErrorMessage=" favor de ingresar una cuenta de correo valida" )]
+        public string Email { get; set;}
+        [Required(ErrorMessage = "Por favor ingrese su numero telefonico.")]
+        public string Phone { get; set;}
+        [Required(ErrorMessage = "Por favor espesifique si va a asistir o no.")]
+        public bool? WillAttend { get; set;}
+    }
+}
